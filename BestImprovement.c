2407901a8c8b163051734d879edd619aa1e0b89e
Luís <luisflavieira@gmail.com>
2017-08-22 21:00:22 -0300

    BestImprovement without removeItem movement

--- BestImprovement.c
+++ BestImprovement.c
@@ -13,27 +13,22 @@ void printKnapsack2(Knapsack knapsack) {
 
 Knapsack BestImprovement(Knapsack knapsack, Item* itens) {
     Knapsack nextKnapsack, currentKnapsack;
-    int delete;
 
     nextKnapsack = copyKnapsack(knapsack);
     currentKnapsack = copyKnapsack(knapsack);
     for (int i = 0; i < knapsack.itensNum; i++) {
-        delete = 1;
         if (addItem(&nextKnapsack, itens[i]) == 1) {
             if (nextKnapsack.profit > currentKnapsack.profit)
                 currentKnapsack = copyKnapsack(nextKnapsack);
+
         }
         else {
             for (int j = 0; j < knapsack.itensNum; j++)
-                if (exchangeItem(&nextKnapsack, itens[i], itens[j]) == 1)
-                    if (nextKnapsack.profit > currentKnapsack.profit) {
+                if (exchangeItem(&nextKnapsack, itens[i], itens[j]) == 1) {
+                    if (nextKnapsack.profit > currentKnapsack.profit)
                         currentKnapsack = copyKnapsack(nextKnapsack);
-                        delete = 0;
-                    }
+                }
         }
-        if (delete == 1)
-            if (removeItem(&nextKnapsack, itens[i]) == 0)
-                printf("Warning! Item wasn't removed!\n");
     }
     return currentKnapsack;
 }
