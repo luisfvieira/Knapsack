7aff27993cb16bda9feff0ed65830533d0433bf2
Luís <luisflavieira@gmail.com>
2017-08-22 21:01:23 -0300

    Deletion function printKnapsack2

--- BestImprovement.c
+++ BestImprovement.c
@@ -1,15 +1,4 @@
-#include <stdio.h>
 #include "knapsack.h"
-
-void printKnapsack2(Knapsack knapsack) {
-    printf("Knapsack\nCapacity: %d\nProfit: %d\nWeight: %d\n", knapsack.capacity,
-        knapsack.profit, knapsack.weight);
-    printf("ItensNum: %d\n", knapsack.itensNum);
-    for (int i = 0; i < knapsack.itensNum; i++) {
-        printf("%d ", knapsack.itens[i]);
-    }
-    printf("\n");
-}
 
 Knapsack BestImprovement(Knapsack knapsack, Item* itens) {
     Knapsack nextKnapsack, currentKnapsack;
