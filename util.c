7f5128ee199e9f69378034ed31430aff50bff55f
Luis Vieira <luisflavieira@gmail.com>
2017-08-24 15:52:52 -0300

    Impl Func returnTime

--- util.c
+++ util.c
@@ -1,12 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+/**
+    Retorna o valor do tempo
+*/
+double returnTime(clock_t initialTime) {
+    clock_t actual = clock();
+    return ((actual - initialTime) / CLOCKS_PER_SEC);
+}
+
 /**
     Verifica se o tempo limite foi atingido.
 */
 int checktime(double timeLimit, clock_t initialTime) {
-    clock_t finalTime = clock();
-    double dTime = ((finalTime - initialTime) * 1000.0) / CLOCKS_PER_SEC;
+    double dTime = returnTime(initialTime);
 
     if (dTime < timeLimit)
         return 1;
